6f86d1a46df80968496479c93deed9659cbfc5c7
Peerapat_sut <89067174+Kurigaya@users.noreply.github.com>
2024-05-25 13:42:45 +0700

    add Mangum to main to handler app for aws

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from router.users import router as users_router
 from router.devices import router as devices_router
 from router.admin import router as admin_router
-from database import db
+from mangum import Mangum
 
 app = FastAPI()
+handler = Mangum(app)
 
 # Allow all origins for local development
 app.add_middleware(
@@ -23,3 +25,6 @@ async def root():
 app.include_router(users_router, prefix="/users", tags=["users"])
 app.include_router(devices_router, prefix="/devices", tags=["devices"])
 app.include_router(admin_router, prefix='/admin', tags=["admin"])
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port="8000", reload=True)
